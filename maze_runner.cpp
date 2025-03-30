46ae0d123c2cfce7dc1adc00bbe9d658e02b40c9
luiz <lhenrique.gsantos@gmail.com>
2025-03-30 11:55:34 -0300

    ajuste de tempo

--- maze_runner.cpp
+++ maze_runner.cpp
@@ -75,7 +75,7 @@ void print_maze() {
         }
         std::cout << std::endl;
     }
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
 }
 
 // Função para verificar se uma posição é válida
